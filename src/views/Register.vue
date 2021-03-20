c613148257e8e19421752c4abf93a307e1661a76
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-03-20 14:32:09 +0000

    abstract registration form

---
+++ src/views/Register.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <RegistrationForm />
+</div>
+  
+</template>
+
+<script>
+import RegistrationForm from '../components/Auth/RegistrationForm.vue'
+export default {
+    name: 'Register',
+    components: {
+        RegistrationForm
+    }
+
+}
+</script>
+
+<style>
+
+</style>
